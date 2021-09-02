86556d48e83aad718c707d14941abbb893f053f2
alexander-krug <62189212+alexander-krug@users.noreply.github.com>
2021-09-02 22:00:51 +0200

    Update webscrape.py

--- webscrape.py
+++ webscrape.py
@@ -1,1 +1,23 @@
-
+#prep
+from selenium import webdriver
+from BeautifulSoup import BeautifulSoup
+import pandas as pd
+driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
+#custom part
+products=[] #List to store name of the product
+prices=[] #List to store price of the product
+ratings=[] #List to store rating of the product
+driver.get("www.flipkart.com/laptops/pr?sid=6bo,b5g&otracker=categorytree")
+#informatoin extraction
+content = driver.page_source
+soup = BeautifulSoup(content)
+for a in soup.findAll('a',href=True, attrs={'class':'_31qSD5'}):
+name=a.find('div', attrs={'class':'_4rR01T'})
+price=a.find('div', attrs={'class':'_1vC4OE _2rQ-NK'})
+rating=a.find('div', attrs={'class':'hGSR34 _2beYZw'})
+products.append(name.text)
+prices.append(price.text)
+ratings.append(rating.text) 
+#data to csv
+df = pd.DataFrame({'Product Name':products,'Price':prices,'Rating':ratings}) 
+df.to_csv('products.csv', index=False, encoding='utf-8')
